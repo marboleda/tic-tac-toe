5216be9e6a70dfa343104cd638354a5fa29e9482
Marco Arboleda <arboleda.marco@gmail.com>
2020-05-07 18:54:46 -0400

    Display game board

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,23 @@ const gameBoard = (() => {
     gameBoardArray = ["x", "o", "x",
                       "x", "o", "x",
                       "o", "x", "o"]
+
+    const render = () => {
+        const gameBoardDiv = document.getElementById("game-board");
+        gameBoardArray.forEach((val) => {
+            const newCell = document.createElement("div");
+            newCell.classList.add("cell");
+            newCell.textContent = val;
+            gameBoardDiv.appendChild(newCell);
+        })
+    }
+
+    return { render }
+
 })();
 
 const playerFactory = () => {
-    
+
 }
+
+gameBoard.render();
