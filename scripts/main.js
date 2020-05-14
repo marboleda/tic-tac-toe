c5d7dbb81e394df29c85eb716e11a38640e9fd13
Marco Arboleda <arboleda.marco@gmail.com>
2020-05-14 16:19:08 -0400

    Implement logic to check if winner has been decided

--- scripts/main.js
+++ scripts/main.js
@@ -23,6 +23,8 @@ const gameBoard = (() => {
                             null, null, null];
     let player1;
     let opponent;
+    let winner = "";
+    let gameInProgress = true;
 
     const addMark = (mark, index) => {
         gameBoardArray[index] = mark;
@@ -44,7 +46,7 @@ const gameBoard = (() => {
         let cellFilledIn = true;
         let cellIndex;
 
-        while (cellFilledIn) {
+        while (cellFilledIn && gameInProgress) {
             cellIndex = Math.floor(Math.random() * 10);
             if (gameBoardArray[cellIndex] === null) {
                 cellFilledIn = false;
@@ -59,7 +61,7 @@ const gameBoard = (() => {
         const cells = document.querySelectorAll(".cell");
         cells.forEach((cell, index) => {
             cell.addEventListener("click", (e) => {
-                if (displayController.getTurnStatus()) {
+                if (displayController.getTurnStatus() && gameInProgress) {
                     cell.textContent = player1.getMark();
                     gameBoardArray[index] = player1.getMark();
                     displayController.setTurnStatus(false);
@@ -69,7 +71,50 @@ const gameBoard = (() => {
         });
     }
 
-    return { addCellListeners, takeComputerTurn, getGameBoardArray, setOpponent, setPlayer1, addMark }
+    const isWinner = (player) => {
+        let playerIsWinner;
+        let winningStates = [[0,1,2],
+                             [3,4,5],
+                             [6,7,8],
+                             [0,3,6],
+                             [1,4,7],
+                             [2,5,8],
+                             [0,4,8],
+                             [2,4,6]];
+
+        for (let i = 0; i < winningStates.length; i++) {
+            playerIsWinner = true;
+            for (let j = 0; j < 3; j++) {
+                if (gameBoardArray[winningStates[i][j]] != player.getMark()) {
+                    playerIsWinner = false;
+                }
+            }
+            if (playerIsWinner) {
+                return true;
+            }
+        }
+        return false;
+
+        /* cannot break out of forEach loop with return statement, 
+         *  needed to convert logic to use for loops as above
+        winningStates.forEach((state) => {
+            let playerIsWinner = true;
+            state.forEach((index) => {
+                if (gameBoardArray[index] != player.getMark()) {
+                    playerIsWinner = false;
+                } 
+            });
+            console.log(playerIsWinner);
+            if (playerIsWinner) {
+                return true;
+            }
+        });
+        return false;
+        */
+
+    };
+
+    return { addCellListeners, takeComputerTurn, getGameBoardArray, setOpponent, setPlayer1, addMark, isWinner }
 
 })();
 
